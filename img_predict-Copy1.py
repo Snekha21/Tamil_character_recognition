f45df8c96212896d8c3f5d18d44dabb074340548
Snekha Suresh <93257859+Snekha21@users.noreply.github.com>
2022-11-03 20:55:15 +0530

    Initial commit

---
+++ img_predict-Copy1.py
@@ -0,0 +1,221 @@
+import os
+import pickle
+import cv2
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import shutil
+from math import floor
+import io
+import requests
+from flask_wtf import FlaskForm
+from flask import request
+from flask import Flask
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email,EqualTo, ValidationError
+from flask import render_template, url_for, flash, redirect, request, abort
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img
+import csv
+from matplotlib import pyplot as plt
+from tensorflow.keras.models import load_model
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import re
+import csv
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
+def getTamilChar(tamilCharacterCode, indx):
+       return tamilCharacterCode[indx]
+tamilCharacterCode = []
+with open('/home/snekha/datasets/tamil_data/unicodeTamil.csv', newline='') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    for i in data:
+        go = i[1].split(' ')
+        charL = ""
+        for gg in go:
+            charL = charL + "\\u"+str(gg)
+        tamilCharacterCode.append(charL.encode('utf-8').decode('unicode-escape'))
+import cv2
+from imutils import contours
+
+# Load image, grayscale, Otsu's threshold
+imag = cv2.imread('sentence.png')
+
+# apply image thresholding
+# thresh = cv2.adaptiveThreshold(imag,    
+#           255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+# invert the image, 255 is the maximum value
+# thresh = 255 — thresh
+gray = cv2.cvtColor(imag, cv2.COLOR_BGR2GRAY)
+# imag = cv2.adaptiveThreshold(imag, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 101, -15)
+thresh = cv2.threshold(gray,0,255,cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
+# imag = align_text(imag)
+# rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 10))
+# threshed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, rect_kernel)
+# ret, thresh = cv2.threshold(imag, 100, 255, cv2.THRESH_TOZERO)
+# thresh = cv2.resize(thresh, (960, 540)) 
+# Find contours, sort from left-to-right, then crop
+cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+cnts, _ = contours.sort_contours(cnts, method="left-to-right")
+
+
+ROI_number = 0
+
+for c in cnts:
+    area = cv2.contourArea(c)
+    if area > 10:
+        x,y,w,h = cv2.boundingRect(c)
+        ROI = 255 - imag[y:y+h, x:x+w]
+        cv2.imwrite('/home/snekha/hackathons/taml/ml/code/image/ROI_{}.png'.format(ROI_number), ROI)
+    
+                # align image text
+                  
+        # image_path = '/home/snekha/hackathons/taml/ml/code/image/ROI_{}.png'.format(ROI_number)
+        # image_file = Image.open(image_path)
+        # image_file.save('/home/snekha/hackathons/taml/ml/code/image/ROI_{}.png'.format(ROI_number), quality=95)
+        cv2.rectangle(imag, (x, y), (x + w, y + h), (36,255,12), 2) 
+        model = load_model('/home/snekha/datasets/tamil_data/tamilALLEzhuthukalKeras_Model.h5')
+        model1 = load_model('/home/snekha/datasets/tamil_data/upscaling.h5')
+        # model = None
+        
+        datasetsLoc = '/home/snekha/hackathons/taml/ml/code/zimg'
+        import PIL 
+        w,h = 128,128
+        i = 0
+        shapeL=[]
+        import cv2
+        # from imutils import contours
+        images=[]
+        labels=[]
+        # from keras.utils.np_utils import to_categorical  
+        # encoder = OneHotEncoder(categories='auto')
+        def bbox2(img1):
+            img = 1 - img1
+            rows = np.any(img, axis=1)
+            cols = np.any(img, axis=0)
+            rmin, rmax = np.where(rows)[0][[0, -1]]
+            cmin, cmax = np.where(cols)[0][[0, -1]]
+            return rmin, rmax, cmin, cmax
+        def RR(img):
+            rmin, rmax, cmin, cmax = bbox2(img)
+            # print(rmin, rmax, cmin, cmax)
+            npArr = img[rmin:rmax, cmin:cmax]
+            npArr = cv2.resize(npArr, dsize=(100, 100))
+            jinga = np.ones((128,128))
+            jinga[14:114,14:114] = npArr
+            npArr = jinga.reshape(128, 128 , 1)
+            return npArr
+
+        def getTamilChar(tamilCharacterCode, indx):
+            return tamilCharacterCode[indx]
+
+        def plotIm(img_):
+          plt.imshow(img_, cmap='gray')
+          plt.show()
+
+        def init_somethings():
+
+            global tamilCharacterCode, model
+
+            with open('/home/snekha/datasets/tamil_data/unicodeTamil.csv', newline='') as f:
+                reader = csv.reader(f)
+                data = list(reader)
+                for i in data:
+                    go = i[1].split(' ')
+                    charL = ""
+                    for gg in go:
+                        charL = charL + "\\u"+str(gg)
+                        tamilCharacterCode.append(charL.encode('utf-8').decode('unicode-escape'))
+
+        # for folders in os.listdir(datasetsLoc):
+            # for files in  os.listdir(datasetsLoc):   
+        def load_image(path, downfactor=3):
+            original_img = load_img(path)
+            downscaled = original_img.resize((original_img.size[0] // downfactor,
+                original_img.size[1] // downfactor), Image.BICUBIC)
+            return (original_img, downscaled)
+
+        def get_y_channel(image):
+            ycbcr = image.convert("YCbCr")
+            (y, cb, cr) = ycbcr.split()
+            y = np.array(y)
+            y = normalize(y.astype("float32"))
+            return (y, cb, cr)
+
+        def upscale_image(img, model):
+            y, cb, cr = get_y_channel(img)
+            input = np.expand_dims(y, axis=0)
+            out = model.predict(input)[0]
+            out *= 255.0
+            out = out.clip(0, 255)
+            out = out.reshape((np.shape(out)[0], np.shape(out)[1]))
+            new_y = Image.fromarray(np.uint8(out), mode="L") # it will generate y channel of image
+            new_cb = cb.resize(new_y.size, PIL.Image.BICUBIC) 
+            new_cr = cr.resize(new_y.size, PIL.Image.BICUBIC)
+            res = PIL.Image.merge("YCbCr", (new_y, new_cb, new_cr)).convert(
+                "RGB"
+            ) # it will convert from YCbCr to RGB image
+            return res
+        # test_path="/home/snekha/hackathons/taml/ml/code/image//ROI_{}.png".format(ROI_number)
+        # # for file_name in os.listdir("/home/snekha/hackathons/taml/ml/code/image/"):
+        # # test_path = os.path.join(test_dir, file_name)
+        # original_img, downscaled = load_image(test_path)
+        # res = upscale_image(downscaled, model)
+        # cv2.imwrite(test_path, res)
+        # the default
+        # image_path = '/home/snekha/hackathons/taml/ml/code/image/ROI_{}.png'.format(ROI_number)
+        # image_file = Image.open(image_path)
+        # image_file.save('/home/snekha/hackathons/taml/ml/code/image/ROI_{}.png'.format(ROI_number), quality=95)
+        for i in range(17):
+            images=[]
+            image = Image.open(datasetsLoc+'/'+'{}.png'.format(i+1))
+            thresh = 200
+            fn = lambda x : 255 if x > thresh else 0
+            image=image.convert('L').point(fn, mode='1')
+            img=np.asarray(image, dtype=np.uint8)
+            shapeL.append(img.shape)
+            img2 = RR(img)
+            img2 = np.asarray(img2, dtype=np.uint8)
+                        # img2 = encoder.fit_transform(img2)
+            images.append(img2)
+
+            # filIm = open('/home/snekha/hackathons/taml/ml/code/new_img/image_ALL_128x128.obj', 'wb')
+            # pickle.dump(images, filIm)
+            # numCategory = 156
+            # filIm = open('/home/snekha/hackathons/taml/ml/code/new_img/image_ALL_128x128.obj', 'rb')
+            # images = pickle.load(filIm)
+            npArr=np.array(images)
+
+            # print(images)
+            # npArr=images[0]
+            # npArr = np.asarray(images, dtype=np.uint8)
+            npArr = npArr.reshape(1, 128, 128 , 1)
+            # print(npArr)
+            # # npArr=images
+            atc = model.predict(npArr)
+
+            percentage = atc[0]
+            valsss = atc[0].argsort()[-3:][::-1]
+            responseTextSt = getTamilChar(tamilCharacterCode,valsss[0])+","+ getTamilChar(tamilCharacterCode,valsss[1])+ ","+ getTamilChar(tamilCharacterCode,valsss[2])
+            responseTextSt = responseTextSt + ',%.3f,%.3f,%.3f'%(percentage[valsss[0]] *100.0,percentage[valsss[1]] *100.0,percentage[valsss[2]]*100.0)
+            # print(responseTextSt)
+            a=getTamilChar(tamilCharacterCode,valsss[0])
+            print(a)
+            file1 = open("tamil_pdf.txt", "a")  # append mode
+            file1.write(a)
+            file1.close()
+            ROI_number += 1
---
+++ routes.py
@@ -0,0 +1,335 @@
+from flask import Flask, request, url_for, redirect, render_template
+import pandas as pd
+import json 
+# from json import jsonify
+from werkzeug.utils import secure_filename
+import pathlib
+import pickle
+from PIL import Image
+import cv2 
+import numpy as np
+# from chatterbot import ChatBot
+# from chatterbot.trainers import ChatterBotCorpusTrainer
+import email, smtplib, ssl
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import random
+# from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+# app.config['MYSQL_DB'] = 'flask'
+ 
+# mysql = MySQL(app)
+ 
+# #Creating a connection cursor
+# cursor = mysql.connection.cursor()
+ 
+# #Executing SQL Statements
+# cursor.execute(''' CREATE TABLE patient_data(name,age,gender,profession,mailid,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12) ''')
+# # cursor.execute(''' INSERT INTO table_name VALUES(v1,v2...) ''')
+# # cursor.execute(''' DELETE FROM table_name WHERE condition ''')
+ 
+# #Saving the Actions performed on the DB
+# mysql.connection.commit()
+ 
+# #Closing the cursor
+# cursor.close()
+
+# subject = "Your Depression Test Report"
+# body = "Hey there! Here is your test report for Depression predction from Blue's Meter"
+# sender_email = "snekhasuresh2777@gmail.com"
+# receiver_email = "bluesmeter@gmail.com"
+# password = "ffomorgrzxnezxdj"
+# message = MIMEMultipart()
+# message["From"] = sender_email
+# message["To"] = receiver_email
+# message["Subject"] = subject
+# message["Bcc"] = receiver_email
+# message.attach(MIMEText(body, "plain"))
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
+
+# @app.route('/home', methods=['GET', 'POST'])
+# def index_func():
+#     if request.method == 'POST':
+#         # do stuff when the form is submitted
+#         # redirect to end the POST handling
+#         # the redirect can be to the same route or somewhere else
+#         return redirect(url_for('home'))
+#     # show the form, it wasn't submitted
+#     return render_template('home.html')
+
+# @app.route('/about', methods=['GET', 'POST'])
+# def about_func():
+#     if request.method == 'POST':
+#         # do stuff when the form is submitted
+#         # redirect to end the POST handling
+#         # the redirect can be to the same route or somewhere else
+#         return redirect(url_for('About'))
+#     # show the form, it wasn't submitted
+#     return render_template('about.html')
+
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login_func():
+#     if request.method == 'POST':
+#         # do stuff when the form is submitted
+#         # redirect to end the POST handling
+#         # the redirect can be to the same route or somewhere else
+#         return redirect(url_for('Login'))
+#     # show the form, it wasn't submitted
+#     return render_template('login.html')
+
+
+# @app.route('/main', methods=['GET', 'POST'])
+# def main_func():
+#     if request.method == 'POST':
+#         # do stuff when the form is submitted
+#         # redirect to end the POST handling
+#         # the redirect can be to the same route or somewhere else
+#         return redirect(url_for('Main'))
+#     # show the form, it wasn't submitted
+#     return render_template('Main.html')
+
+# @app.route('/contact', methods=['GET', 'POST'])
+# def contact_func():
+#     if request.method == 'POST':
+#         # do stuff when the form is submitted
+#         # redirect to end the POST handling
+#         # the redirect can be to the same route or somewhere else
+#         return redirect(url_for('Contact'))
+#     # show the form, it wasn't submitted
+#     return render_template('contact.html')
+
+# # @app.route('/result', methods=['GET', 'POST'])
+# # def result_func():
+# #     if request.method == 'POST':
+# #         # do stuff when the form is submitted
+# #         # redirect to end the POST handling
+# #         # the redirect can be to the same route or somewhere else
+# #         return redirect(url_for('result'))
+# #     # show the form, it wasn't submitted
+# #     return render_template('result.html')
+
+# @app.route('/input', methods=['GET', 'POST'])
+# def test_func():
+#     if request.method == 'POST':
+#         # data = request.form.get("q1")
+#         # for key, value in request.form.items():
+#         #     print("key: {0}, value: {1}".format(key, value))
+
+#         # data=console.log(JSON.stringify(input))
+#         # data1 =json.loads(request.json)
+#         # data = request.form.get("q1")
+#         # data = request.json
+#         data= request.form
+#         # data = json.dump(request.get_json(force=True))
+#         # with open('file.json', 'w') as f:
+#         #   json.dump(request.get_json(force=True), f)
+#         # do stuff when the form is submitted
+#         # redirect to end the POST handling
+#         # the redirect can be to the same route or somewhere else
+#         print(data)
+#         # print(data1)
+#         convertJsonToCsv(data)
+#         return redirect(url_for('result'))
+#         # return render_template('result.html')
+#     return render_template('index.html')
+#     # show the form, it wasn't submitted
+#     # return render_template('index.html')
+
+@app.route("/image")
+def image():
+#   if request.method == "POST":
+#     image = request.files.get('img', '')
+#     image = request.form["img"]
+
+#     print("img ",image)
+#     return "<h1> yo! </h1>"
+#   else:
+    return render_template('login.html') 
+
+@app.route('/upload', methods = ['POST', 'GET'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['File']
+        f.save(secure_filename("tamil_pdff.pdf"))
+        
+        
+        # if mri()==1:
+        #     html_data= "You are depressed"
+        #     result = "You are showing symptoms of depression"
+        #     cmt= "You are adviced to contact your doctor and get treated asap"
+        #     filename = "depressed.pdf"
+        # else:
+        #     html_data = "You are not depressed"
+        #     result = "Congrats! You are not showing symptoms of depression"
+        #     cmt = "Stay happy and healthy"
+        #     filename="healthy.pdf"
+        # with open(filename, "rb") as attachment:
+        #      part = MIMEBase("application", "octet-stream")
+        #      part.set_payload(attachment.read())
+        #      encoders.encode_base64(part)
+        #      part.add_header(
+        #     "Content-Disposition",
+        #      f"attachment; filename= {filename}",
+        #      )
+        #      message.attach(part)
+        #      text = message.as_string()
+        #      context = ssl.create_default_context()
+        #      with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        #         server.login(sender_email, password)
+        #     # msg = Message('Hello', sender = 'snekhasuresh2777@gmail.com', recipients = ['bluesmeter@gmail.com'])
+        #     # msg.body = "Hello Flask message sent from Flask-Mail"
+        #     # mail.send(msg)
+        #         server.sendmail(sender_email, receiver_email, text)
+        return render_template("home.html")
+        
+# @app.route("/test", methods = ['POST','GET'])
+# def login():
+#     if request.method == 'POST':
+#         return redirect(url_for('test'))
+    
+#     return render_template('index.html')
+
+
+# def convertJsonToCsv(jsonData):
+#     dfs = []
+#     dfs.append(pd.DataFrame([jsonData]))
+#     df = pd.concat(dfs, ignore_index=True, sort=False)
+#     # df.drop(['userName'], 
+#     #     axis = 1, inplace = True)
+#     df.to_csv('input.csv',index=False)
+
+# @app.route("/result")
+# def result():
+#     data_dir = pathlib.Path("input.csv")
+#     df=pd.read_csv(data_dir)
+#     data1= df.transpose()
+#     # try x=data1[0].value_counts()[0]
+#     x=data1[0].value_counts()[0]
+#     y=data1[0].value_counts()[1]
+#     z=data1[0].value_counts()[2]
+#     n= (x/24)*100
+#     s= (y/24)*100
+#     a= (z/24)*100
+#     d=s+a
+#     # data1['Average'] = data1.mean(axis=1)
+#     # data1.drop(['Monday','Tuesday','Wednesday','Thursday','Friday'],axis = 1, inplace = True)
+#     # fmodel = pickle.load(open('fin_model.pkl','rb'))
+#     # model = pickle.load(open('fin_model.pkl','rb'))
+#     # if(model.predict(data1) == 0):
+#     #     return  {"output":"You are depressed"}
+#     # else:
+#     #     return {"output":"You are not depressed"}
+#     # data1 = data1.astype(float)
+#     model = pickle.load(open('lr_model.pkl','rb'))
+#     if(model.predict(df) == 1):
+#         if (d<5):
+#             html_data= "No depression"
+#             result = "Congrats! You are living a healthy life!"
+#             cmt= "Stay happy and healthy"
+#         elif (d<25):
+#             html_data= "Beginner Stage"
+#             result = "You had just started showing the symptoms of depression"
+#             cmt= "Just wake up! It's not too late..."
+#         elif (d<65):
+#             html_data= "Moderate Stage"
+#             result = "You are showing mild symptoms of depression and adviced to consult a psychiatrist asap"
+#             cmt= "There is hope, even when your brain tells you there isn't.."
+#         elif (d>65):
+#             html_data= "Severe Stage"
+#             result = "You are adviced to visit a psychiatrist asap and get necessary treatment, contact help-desk to reach out to doctors"
+#             cmt= "To confirm the severity of depression, kindly take a DTI-MRI scan"
+#         filename = "depressed.pdf"
+#     elif(model.predict(df) == 0):
+#         html_data = "You are not depressed"
+#         result = "Congrats! You are living a healthy life!"
+#         cmt = "Stay happy and healthy"
+#         filename="healthy.pdf"
+
+#     with open(filename, "rb") as attachment:
+#              part = MIMEBase("application", "octet-stream")
+#              part.set_payload(attachment.read())
+#              encoders.encode_base64(part)
+#              part.add_header(
+#             "Content-Disposition",
+#              f"attachment; filename= {filename}",
+#              )
+#              message.attach(part)
+#              text = message.as_string()
+#              context = ssl.create_default_context()
+#              with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+#                 server.login(sender_email, password)
+#             # msg = Message('Hello', sender = 'snekhasuresh2777@gmail.com', recipients = ['bluesmeter@gmail.com'])
+#             # msg.body = "Hello Flask message sent from Flask-Mail"
+#             # mail.send(msg)
+#                 server.sendmail(sender_email, receiver_email, text)
+#     print(model.predict(df))
+#     return render_template("result.html", html_data = html_data , result=result, cmt=cmt)
+
+# @app.route("/img")
+# def mri():
+#         # img = Image.open("image.jpg")
+#         # img = img.convert("L")
+#         img_array=cv2.imread("image.jpg")
+#         image = cv2.resize(img_array, (100,100))
+#         X= np.array(image).reshape(1,-1)
+#         # svc.predict(X)
+# #       test=[]
+# #       img_array=cv2.imread("image.jpg")
+# #       CATEGORIES = ['healthy','depressed']
+# #       class_num=CATEGORIES.index(category)
+# #       new_array=cv2.resize(img_array,(100,100))
+# #       test.append([new_array,class_num])
+# #     # data1['Average'] = data1.mean(axis=1)
+# #     # data1.drop(['Monday','Tuesday','Wednesday','Thursday','Friday'],axis = 1, inplace = True)
+# #     # fmodel = pickle.load(open('fin_model.pkl','rb'))
+# #     # model = pickle.load(open('fin_model.pkl','rb'))
+# #     # if(model.predict(data1) == 0):
+# #     #     return  {"output":"You are depressed"}
+# #     # else:
+# #     #     return {"output":"You are not depressed"}
+# #     # data1 = data1.astype(float)
+#         model = pickle.load(open('image.pkl','rb'))
+#         # if(model.predict(X) == 1):
+#         #     html_data= "You are depressed"
+#         #     result = "You are showing symptoms of depression"
+#         #     cmt= "You are adviced to contact your doctor and get treated asap"
+#         # elif(model.predict(X) == 0):
+#         #     html_data = "You are not depressed"
+#         #     result = "Congrats! You are not showing symptoms of depression"
+#         #     cmt = "Stay happy and healthy"
+#         # print(model.predict(X))
+#         # return (html_data , result, cmt)
+#         return(model.predict(X))
+
+# english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+# trainer = ChatterBotCorpusTrainer(english_bot)
+# trainer.train("chatterbot.corpus.english")
+# response = english_bot.get_response('What is depression?')
+# print(response)
+ 
+# response = english_bot.get_response('Who are you?')
+# print(response)
+ 
+# @app.route("/chat")
+# def chat():
+#     return render_template("chat.html")
+ 
+# @app.route("/get")
+# def get_bot_response():
+#     userText = request.args.get('msg')
+#     return str(english_bot.get_response(userText))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8000,debug=True)
+
+
+    
